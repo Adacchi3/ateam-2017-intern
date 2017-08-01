0bf254fe1b3498f19f2061c3814d52f5d6e6baf5
Adacchi3 <adachi.prog@gmail.com>
2017-08-01 15:35:35 +0900

    set the route of blogs/like

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :replies
+  resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'blogs/:id/like' => 'blogs#like'
 end
